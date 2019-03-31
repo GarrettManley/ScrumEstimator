82333094dc36719368eb50d1ed7504f3b6755ee5
Garrett Manley <garrett.manley@perficient.com>
2019-03-31 17:00:32 -0500

    Added firebase service basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'prft-est-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ScrumEstimator';
+
+  constructor() {}
 }
