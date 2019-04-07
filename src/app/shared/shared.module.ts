04991811d8bd8053bd695055754af176d007c343
Garrett Manley <garrett.manley@perficient.com>
2019-04-06 23:42:05 -0500

    Added button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CardGroupComponent } from './card-group/card-group.component';
 import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [NavBarComponent, CardGroupComponent, CardComponent],
+  declarations: [NavBarComponent, CardGroupComponent, CardComponent, ButtonComponent],
   imports: [CommonModule],
   exports: [NavBarComponent, CardGroupComponent, CardComponent],
 })
