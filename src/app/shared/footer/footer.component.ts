e6042c056a43914ae071b705408efaf12cf72179
Garrett Manley <garrett.manley@perficient.com>
2019-04-07 01:57:20 -0500

    Added footer

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'scrum-est-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent implements OnInit {
+  public version: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.version = environment.version;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CardGroupComponent } from './card-group/card-group.component';
 import { CardComponent } from './card/card.component';
 import { ButtonComponent } from './button/button.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavBarComponent, CardGroupComponent, CardComponent, ButtonComponent],
+  declarations: [NavBarComponent, CardGroupComponent, CardComponent, ButtonComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [NavBarComponent, CardGroupComponent, CardComponent, ButtonComponent],
+  exports: [NavBarComponent, CardGroupComponent, CardComponent, ButtonComponent, FooterComponent],
 })
 export class SharedModule {}
