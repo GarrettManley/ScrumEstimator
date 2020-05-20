7c009d34691830dc6cc9aa60b3ed0d00c326f260
Garrett Manley <garrett.manley@perficient.com>
2020-05-20 09:30:06 -0500

    added back header animation

--- src/app/page/voting-room/voting-room.component.ts
+++ src/app/page/voting-room/voting-room.component.ts
@@ -1,30 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Card } from 'src/app/shared/models/card/card.model';
-import { SelectableGroup } from 'src/app/shared/models/base/base.selectable-group.model';
-import { CardTypes } from 'src/app/shared/models/card/card-types.enum';
-import { Button } from 'src/app/shared/models/button/button.model';
+import { Component, OnInit } from "@angular/core";
+import { Card } from "src/app/shared/models/card/card.model";
+import { SelectableGroup } from "src/app/shared/models/base/base.selectable-group.model";
+import { CardTypes } from "src/app/shared/models/card/card-types.enum";
+import { Button } from "src/app/shared/models/button/button.model";
 
 @Component({
-  selector: 'scrum-est-voting-room',
-  templateUrl: './voting-room.component.html',
-  styleUrls: ['./voting-room.component.scss'],
+  selector: "scrum-est-voting-room",
+  templateUrl: "./voting-room.component.html",
+  styleUrls: ["./voting-room.component.scss"],
 })
 export class VotingRoomComponent implements OnInit {
   // voting group
   votingGroup: SelectableGroup;
-  votingGroups: Button[] = [new Button('Developers'), new Button('QA'), new Button('Designers')];
+  votingGroups: Button[] = [
+    new Button("Developers"),
+    new Button("QA"),
+    new Button("Designers"),
+  ];
 
   // voting cards
   votingCardGroup: SelectableGroup;
   votingCards: Card[] = [
-    new Card('0', CardTypes.interactive),
-    new Card('1', CardTypes.interactive),
-    new Card('2', CardTypes.interactive),
-    new Card('3', CardTypes.interactive),
-    new Card('5', CardTypes.interactive),
-    new Card('8', CardTypes.interactive),
-    new Card('13', CardTypes.interactive),
-    new Card('?', CardTypes.interactive),
+    new Card("0", CardTypes.interactive),
+    new Card("1", CardTypes.interactive),
+    new Card("2", CardTypes.interactive),
+    new Card("3", CardTypes.interactive),
+    new Card("5", CardTypes.interactive),
+    new Card("8", CardTypes.interactive),
+    new Card("13", CardTypes.interactive),
+    new Card("?", CardTypes.interactive),
   ];
 
   ngOnInit() {
@@ -33,12 +37,15 @@ export class VotingRoomComponent implements OnInit {
   }
 
   setupVotingGroup() {
-    const votingGroupTitle = 'Choose a voting group';
+    const votingGroupTitle = "Choose a voting group";
     this.votingGroup = new SelectableGroup(this.votingGroups, votingGroupTitle);
   }
 
   setupVotingCards() {
-    const votingCardGroupTitle = 'Vote for a story point estimate';
-    this.votingCardGroup = new SelectableGroup(this.votingCards, votingCardGroupTitle);
+    const votingCardGroupTitle = "Vote for a story point estimate";
+    this.votingCardGroup = new SelectableGroup(
+      this.votingCards,
+      votingCardGroupTitle
+    );
   }
 }
